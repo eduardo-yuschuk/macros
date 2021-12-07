fefc74188a98b2011573e62a9c3f64056e53e3a2
eduardo.yuschuk <eduardo.yuschuk@gmail.com>
2021-12-06 23:52:59 -0300

    -

--- declarative/src/main.rs
+++ declarative/src/main.rs
@@ -1,24 +1,31 @@
-struct Minteable {
+struct Base {}
+
+impl Base {
+    fn execute() -> i32 {
+        println!("Base executed");
+        0
+    }
 }
+
+struct Minteable {}
 
 impl Minteable {
-	fn execute() -> i32 {
-		println!("Minteable executed");
-		0
-	}
+    fn execute() -> i32 {
+        println!("Minteable executed");
+        12
+    }
 }
 
-struct Burnable {
-}
+struct Burnable {}
 
 impl Burnable {
-	fn execute() -> i32 {
-		println!("Burnable executed");
-		32
-	}
+    fn execute() -> i32 {
+        println!("Burnable executed");
+        32
+    }
 }
 
-macro_rules! define_contract {
+macro_rules! compose {
 	($name:ident($($parent:ident),+) {$($field_name:ident : $field_type:ty),+}) => {
 		#[allow(dead_code)]
 		#[derive(Debug)]
@@ -30,11 +37,57 @@ macro_rules! define_contract {
 			}
 		}
 	};
+	($name:ident($($parent:ident),+) {$($field_name:ident : $field_type:ty),+} $custom:expr) => {
+		#[allow(dead_code)]
+		#[derive(Debug)]
+		struct $name { $($field_name: $field_type),+ }
+		impl $name {
+			fn execute(&self) {
+				let results = [$($parent :: execute()),+, $custom()];
+				println!("{} executed, internals {:?}, results {:?}", stringify!($name), self, results);
+			}
+		}
+	};
 }
 
-define_contract! { CsdScript(Minteable,Burnable) { sample_data: i32 } }
-
 fn main() {
-	let contract = CsdScript { sample_data: 13i32 };
-	contract.execute();
+    {
+		println!("--------------------------------------------------------------------------------");
+        println!("Testing CsdScript(Base)...");
+        compose! { CsdScript(Base) { sample_data: i32 } }
+        let contract = CsdScript { sample_data: 13i32 };
+        contract.execute();
+    }
+	{
+		println!("--------------------------------------------------------------------------------");
+        println!("Testing CsdScript(Minteable)...");
+        compose! { CsdScript(Minteable) { sample_data: i32 } }
+        let contract = CsdScript { sample_data: 13i32 };
+        contract.execute();
+    }
+	{
+		println!("--------------------------------------------------------------------------------");
+        println!("Testing CsdScript(Burnable)...");
+        compose! { CsdScript(Burnable) { sample_data: i32 } }
+        let contract = CsdScript { sample_data: 13i32 };
+        contract.execute();
+    }
+	{
+		println!("--------------------------------------------------------------------------------");
+        println!("Testing CsdScript(Base,Minteable,Burnable)...");
+        compose! { CsdScript(Base,Minteable,Burnable) { sample_data: i32 } }
+        let contract = CsdScript { sample_data: 13i32 };
+        contract.execute();
+    }
+	{
+		println!("--------------------------------------------------------------------------------");
+        println!("Testing CsdScript(Base) |custom| ...");
+        compose! { CsdScript(Base,Minteable,Burnable) { sample_data: i32 } || { 
+			println!("Custom executed");
+        	1
+		}}
+        let contract = CsdScript { sample_data: 13i32 };
+        contract.execute();
+    }
+	println!("--------------------------------------------------------------------------------");
 }
